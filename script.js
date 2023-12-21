9d9ca040da721bb67400f9879dd837e3538a116b
DamianJM <damianmagill@hotmail.co.uk>
2023-12-21 16:33:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,123 +1,102 @@
-const CANVAS_SIZE = 280;
-const CANVAS_SCALE = 0.5;
+(function() {
 
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-const clearButton = document.getElementById("clear-button");
+        var width = 320;
+        var height = 0;
 
-let isMouseDown = false;
-let hasIntroText = true;
-let lastX = 0;
-let lastY = 0;
+        var streaming = false;
 
-// Load our model.
-const sess = new onnx.InferenceSession();
-const loadingModelPromise = sess.loadModel("./onnx_model.onnx");
+        var video = null;
+        var canvas = null;
+        var photo = null;
+        var startbutton = null;
+        var downloadbutton = null;
 
-// Add 'Draw a number here!' to the canvas.
-ctx.lineWidth = 28;
-ctx.lineJoin = "round";
-ctx.font = "28px sans-serif";
-ctx.textAlign = "center";
-ctx.textBaseline = "middle";
-ctx.fillStyle = "#212121";
-// ctx.fillText("Draw a number here!", CANVAS_SIZE / 2, CANVAS_SIZE / 2);
-ctx.fillText("Loading...", CANVAS_SIZE / 2, CANVAS_SIZE / 2);
+        // Load our model.
+        const sess = new onnx.InferenceSession();
+        const loadingModelPromise = sess.loadModel("./model.onnx");
 
-// Set the line color for the canvas.
-ctx.strokeStyle = "#212121";
+        function startup() {
+            video = document.getElementById('video');
+            canvas = document.getElementById('canvas');
+            photo = document.getElementById('photo');
+            startbutton = document.getElementById('startbutton');
+            downloadbutton = document.getElementById('downloadbutton');
+            clearbutton = document.getElementById('clearbutton')
+            navigator.mediaDevices.getUserMedia({
+                    video: true,
+                    audio: false
+                })
+                .then(function(stream) {
+                    video.srcObject = stream;
+                    video.play();
+                })
+                .catch(function(err) {
+                    console.log("An error occurred: " + err);
+                });
 
-function clearCanvas() {
-  ctx.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
-  for (let i = 0; i < 10; i++) {
-    const element = document.getElementById(`prediction-${i}`);
-    element.className = "prediction-col";
-    element.children[0].children[0].style.height = "0";
-  }
-}
+            video.addEventListener('canplay', function(ev) {
+                if (!streaming) {
+                    height = video.videoHeight / (video.videoWidth / width);
 
-function drawLine(fromX, fromY, toX, toY) {
-  // Draws a line from (fromX, fromY) to (toX, toY).
-  ctx.beginPath();
-  ctx.moveTo(fromX, fromY);
-  ctx.lineTo(toX, toY);
-  ctx.closePath();
-  ctx.stroke();
-  updatePredictions();
-}
+                    if (isNaN(height)) {
+                        height = width / (4 / 3);
+                    }
 
-async function updatePredictions() {
-  // Get the predictions for the canvas data.
-  const imgData = ctx.getImageData(0, 0, CANVAS_SIZE, CANVAS_SIZE);
-  const input = new onnx.Tensor(new Float32Array(imgData.data), "float32");
+                    video.setAttribute('width', width);
+                    video.setAttribute('height', height);
+                    canvas.setAttribute('width', width);
+                    canvas.setAttribute('height', height);
+                    streaming = true;
+                }
+            }, false);
 
-  const outputMap = await sess.run([input]);
-  const outputTensor = outputMap.values().next().value;
-  const predictions = outputTensor.data;
-  const maxPrediction = Math.max(...predictions);
+            startbutton.addEventListener('click', function(ev) {
+                takepicture();
+                ev.preventDefault();
+            }, false);
 
-  for (let i = 0; i < predictions.length; i++) {
-    const element = document.getElementById(`prediction-${i}`);
-    element.children[0].children[0].style.height = `${predictions[i] * 100}%`;
-    element.className =
-      predictions[i] === maxPrediction
-        ? "prediction-col top-prediction"
-        : "prediction-col";
-  }
-}
+            downloadbutton.addEventListener('click', function() {
+                inferObject();
+            });
 
-function canvasMouseDown(event) {
-  isMouseDown = true;
-  if (hasIntroText) {
-    clearCanvas();
-    hasIntroText = false;
-  }
-  const x = event.offsetX / CANVAS_SCALE;
-  const y = event.offsetY / CANVAS_SCALE;
+            clearbutton.addEventListener('click', function() {
+                clearphoto();
+            });
+            clearphoto();
+        }
 
-  // To draw a dot on the mouse down event, we set laxtX and lastY to be
-  // slightly offset from x and y, and then we call `canvasMouseMove(event)`,
-  // which draws a line from (laxtX, lastY) to (x, y) that shows up as a
-  // dot because the difference between those points is so small. However,
-  // if the points were the same, nothing would be drawn, which is why the
-  // 0.001 offset is added.
-  lastX = x + 0.001;
-  lastY = y + 0.001;
-  canvasMouseMove(event);
-}
+        function clearphoto() {
+            var context = canvas.getContext('2d');
+            context.fillStyle = "#AAA";
+            context.fillRect(0, 0, canvas.width, canvas.height);
 
-function canvasMouseMove(event) {
-  const x = event.offsetX / CANVAS_SCALE;
-  const y = event.offsetY / CANVAS_SCALE;
-  if (isMouseDown) {
-    drawLine(lastX, lastY, x, y);
-  }
-  lastX = x;
-  lastY = y;
-}
+            var data = canvas.toDataURL('image/png');
+            photo.setAttribute('src', data);
+        }
 
-function bodyMouseUp() {
-  isMouseDown = false;
-}
+        function takepicture() {
+            var context = canvas.getContext('2d');
+            if (width && height) {
+                canvas.width = width;
+                canvas.height = height;
+                context.drawImage(video, 0, 0, width, height);
 
-function bodyMouseOut(event) {
-  // We won't be able to detect a MouseUp event if the mouse has moved
-  // ouside the window, so when the mouse leaves the window, we set
-  // `isMouseDown` to false automatically. This prevents lines from
-  // continuing to be drawn when the mouse returns to the canvas after
-  // having been released outside the window.
-  if (!event.relatedTarget || event.relatedTarget.nodeName === "HTML") {
-    isMouseDown = false;
-  }
-}
+                var data = canvas.toDataURL('image/png');
+                photo.setAttribute('src', data);
+            } else {
+                clearphoto();
+            }
+        }
 
-loadingModelPromise.then(() => {
-  canvas.addEventListener("mousedown", canvasMouseDown);
-  canvas.addEventListener("mousemove", canvasMouseMove);
-  document.body.addEventListener("mouseup", bodyMouseUp);
-  document.body.addEventListener("mouseout", bodyMouseOut);
-  clearButton.addEventListener("mousedown", clearCanvas);
+        function downloadPhoto() {
+            var dataUrl = canvas.toDataURL('image/png');
 
-  ctx.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
-  ctx.fillText("Draw a number here!", CANVAS_SIZE / 2, CANVAS_SIZE / 2);
-})
+            var a = document.createElement('a');
+            a.href = dataUrl;
+            a.download = 'webcam_photo.png';
+
+            a.click();
+        }
+
+        window.addEventListener('load', startup, false);
+    })();
