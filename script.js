b27fa573b8429a527a28f1afa78b04bfac011085
DamianJM <damianmagill@hotmail.co.uk>
2023-12-21 16:45:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 (function() {
 
+        const CANVAS_SIZE = 280;
+        const CANVAS_SCALE = 0.5;
+
+        const canvas = document.getElementById("canvas");
+        const ctx = canvas.getContext("2d");
+
         var width = 320;
         var height = 0;
 
         var streaming = false;
 
         var video = null;
-        var canvas = null;
+        //var canvas = null;
         var photo = null;
         var startbutton = null;
         var downloadbutton = null;
@@ -14,6 +20,28 @@
         // Load our model.
         const sess = new onnx.InferenceSession();
         const loadingModelPromise = sess.loadModel("model.onnx");
+
+        // predictions on model
+
+        async function updatePredictions() {
+            // Get the predictions for the canvas data.
+            const imgData = ctx.getImageData(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+            const input = new onnx.Tensor(new Float32Array(imgData.data), "float32");
+          
+            const outputMap = await sess.run([input]);
+            const outputTensor = outputMap.values().next().value;
+            const predictions = outputTensor.data;
+            const maxPrediction = Math.max(...predictions);
+          
+            for (let i = 0; i < predictions.length; i++) {
+              const element = document.getElementById(`prediction-${i}`);
+              element.children[0].children[0].style.height = `${predictions[i] * 100}%`;
+              element.className =
+                predictions[i] === maxPrediction
+                  ? "prediction-col top-prediction"
+                  : "prediction-col";
+            }
+          }
 
         function startup() {
             video = document.getElementById('video');
@@ -56,7 +84,7 @@
             }, false);
 
             downloadbutton.addEventListener('click', function() {
-                inferObject();
+                updatePredictions();
             });
 
             clearbutton.addEventListener('click', function() {
